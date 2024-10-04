b49d48491e601cc911a5f7d4c1120b109f9f6bee
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-10-04 15:35:53 +0100

    Updated as per 'Judge' requirements

--- 01_lists_as_stacks_and_queues/exercise/06_balanced_parentheses.py
+++ 01_lists_as_stacks_and_queues/exercise/06_balanced_parentheses.py
@@ -1,33 +1,25 @@
 from collections import deque
 
+expression = deque(input())
+OPENING_PARENTHESES = '({['
+CLOSING_PARENTHESES = ')}]'
+counter = 0
 
-def find_balanced(text):
-    expression = deque(text)
-    opening_parentheses = '({['
-    closing_parentheses = ')}]'
-    counter = 0
+while expression:
+    if expression[0] not in OPENING_PARENTHESES:
+        break
 
-    while expression:
-        if expression[0] not in opening_parentheses:
-            break
-
-        index = opening_parentheses.index(expression[0])
-        if expression[1] == closing_parentheses[index]:
-            expression.popleft()
-            expression.popleft()
-            expression.rotate(counter)
-            counter = 0
-        else:
-            expression.rotate(-1)
-            counter += 1
-
-    if expression:
-        return "NO"
+    index = OPENING_PARENTHESES.index(expression[0])
+    if expression[1] == CLOSING_PARENTHESES[index]:
+        expression.popleft()
+        expression.popleft()
+        expression.rotate(counter)
+        counter = 0
     else:
-        return "YES"
+        expression.rotate(-1)
+        counter += 1
 
-
-# text = deque(input())
-print(find_balanced('{{[[(())]]}}'))
-print(find_balanced('{[(])}'))
-print(find_balanced('{[()]}'))
+if expression:
+    print("NO")
+else:
+    print("YES")
