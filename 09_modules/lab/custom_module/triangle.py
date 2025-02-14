5f751a1d152d444e759b0e9f201ece0ffa155d52
Zdravko <zdravko.galchov@gmail.com>
2025-02-14 15:59:07 +0000

    Added 'Modules' lab

---
+++ 09_modules/lab/custom_module/triangle.py
@@ -0,0 +1,17 @@
+def triangle(number):
+    for row in range(1, number + 2):
+        result = []
+
+        for num in range(1, row):
+            result.append(str(num))
+
+        print(' '.join(result))
+
+    for row in range(number, 0, -1):
+        result = []
+
+        for num in range(1, row):
+            result.append(str(num))
+
+        print(' '.join(result))
+        
